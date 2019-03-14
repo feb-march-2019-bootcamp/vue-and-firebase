7863e7df914a7756084fa55b2731450a046fb9c4
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-03-14 11:12:11 +0300

    Fixed the syntax error

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,27 +1,27 @@
 <template>
   <form name="contact" @submit.prevent="submit()" action="/" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+    <input type="hidden" name="form-name" value="contact">
 
-      <div class="form-group">
-        Name: <input type="text" name="name" id="name" class="form-control">
-      </div>
-      <div class="form-group">
-        Message: <textarea name="message" id="" cols="30" rows="10" class="form-control"></textarea>
-      </div>
+    <div class="form-group">Name:
+      <input type="text" name="name" id="name" class="form-control">
+    </div>
+    <div class="form-group">Message:
+      <textarea name="message" id cols="30" rows="10" class="form-control"></textarea>
+    </div>
 
-      <div class="form-group">
-        <button type="submit" class="btn btn-success btn-block">Submit</button>
-      </div>
-    </form>
+    <div class="form-group">
+      <button type="submit" class="btn btn-success btn-block">Submit</button>
+    </div>
+  </form>
 </template>
 <script>
 export default {
   data: () => ({
-    name: '',
-    message: ''
+    name: "",
+    message: ""
   }),
   methods: {
-    encode (data) {
+    encode(data) {
       return Object.keys(data)
         .map(
           key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
@@ -29,21 +29,27 @@ export default {
         .join("&");
     },
     submit() {
-      fetch({
-        url: '/',
-        method: 'post',
-        headers: { "Content-Type": "application/x-www-form-urlencoded" }
-      }, { body: 
-        this.encode({
-          'form-name': 'contact',
-          name: this.name,
-          message: this.message
-        }).then(() => {
-        alert('success')
-      }).catch(() => {
-        alert('fail')
-      })
+      fetch(
+        {
+          url: "/",
+          method: "post",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        },
+        {
+          body: this.encode({
+            "form-name": "contact",
+            name: this.name,
+            message: this.message
+          })
+        }
+      )
+        .then(() => {
+          alert("success");
+        })
+        .catch(() => {
+          alert("fail");
+        });
     }
   }
-}
+};
 </script>
